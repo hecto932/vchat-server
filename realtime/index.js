6a6b8ded3f98a3b3e39ecec90cbb49d5b4eb0b49
Hector Flores <hecto932@gmail.com>
2015-12-26 20:22:57 -0430

    Seprando la logica del server del cliente

---
+++ realtime/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const socketio = require('socket.io')
+const database = require('../database')
+const helper = require('../helper')
+
+module.exports = function (server){
+	const db = database()
+	const io = socketio(server)
+
+	io.on('connection', onConnection)
+
+	function onConnection(socket){
+		console.log(`Client connected ${socket.id}`)
+
+		db.list(function(err, messages){
+			if(err) return console.log(err)
+
+			socket.emit('messages', messages)
+		})
+
+		socket.on('message', function(message){
+			const converter = helper.convertVideo(message.frames)
+			converter.on('log', console.log)
+			converter.on('video', function(video){
+				delete message.frames
+				message.video = video
+
+				db.save(message, function(err){
+
+				})
+
+				socket.broadcast.emit('message', message)
+
+				socket.emit('messageack', message)
+			})
+		})
+	}
+}
---
+++ router/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path')
+const course = require('course')
+const st = require('st')
+const jsonBody = require('body/json')
+const vchatClient = require('vchat-client')
+const helper = require('../helper')
+
+const router = course()
+const mount = st({
+  path: vchatClient,
+  index: 'index.html',
+  passthrough: true
+})
+
+
+function onRequest (req, res) {
+  if(req.url.startsWith('/socket.io')) return 
+
+  mount(req, res, function (err) {
+    if (err) return fail(err, res)
+
+    router(req, res, function (err) {
+      if (err) return fail(err, res)
+
+      res.statusCode = 404
+      res.end(`404 Not Found: ${req.url}`)
+    })
+  })
+}
+
+function fail (err, res) {
+  res.statusCode = 500
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(err.message)
+}
+
+module.exports = onRequest
